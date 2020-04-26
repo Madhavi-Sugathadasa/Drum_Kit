68e2dd1a90a7e324cc75d0d6e1827072f3d9d8f4
Madhavi-Sugathadasa <madhavi.sug@gmail.com>
2020-04-26 23:48:47 +1000

    added a missing semicolon

--- index.js
+++ index.js
@@ -62,5 +62,5 @@ function buttonAnimation(currentKey)
     activeButton.classList.add("pressed");
     setTimeout(function (){
         activeButton.classList.remove("pressed");
-    },100)
+    },100);
 }
